ab067fd927869489aa698e65b8bc639cadc42f52
LeonardoLindroth <leolindroth14@gmail.com>
2023-01-10 18:26:14 -0500

    changing app to write html files

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const http = require("node:http");
+const fs = require("node:fs");
 const { hostname } = require("node:os");
 
 const port = "3000";
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Teste");
-    res.end();
+    fs.readFile("./index.html", (error, data) => {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(data);
+        res.end();
+    });
 });
 
 server.listen(port, hostname, () => {
