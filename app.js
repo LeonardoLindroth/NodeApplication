eda91a2ce023e23285faf867d088bc6d45b410fb
LeonardoLindroth <leolindroth14@gmail.com>
2023-01-16 17:19:05 -0500

    added some mimetypes to insert extensions

--- app.js
+++ app.js
@@ -4,19 +4,43 @@ const { hostname } = require("node:os");
 
 const port = "3000";
 
+const MIMETypes = {
+    "txt": "text/plain",
+    "html": "text/html",
+    "css": "text/css",
+    "js": "text/javascript",
+    "default": "application/octet-stream"
+}
+
 const requisitionMethods = {
     get: "GET",
     post: "POST"
 }
 
+const buildPath = (req, res) => {
+    let fullPath = "./views/";
+
+    let path = req.url;
+
+    if (req.url.at(-1) === "/") {
+        path += "index.html";
+    }
+
+    return fullPath + path;
+}
+
 const readFile = (path, req, res) => {
     fs.readFile(path, (error, data) => {
         if (error) {
-            res.writeHead(400, {"Content-Type": "text/plain"})
-            res.write("Error 400: Bad Request");
+            res.writeHead(404, {"Content-Type": "text/plain"})
+            res.write("Error 404: Not Found");
             res.end();
         } else {
-            res.writeHead(200, {"Content-Type": "text/html"});
+            let ext = path.split(".").at(-1);
+
+            res.writeHead(200, 
+                {"Content-Type": ext in MIMETypes ? MIMETypes[ext] : MIMETypes["default"]}
+            );
             res.write(data);
             res.end();
         }
@@ -25,15 +49,13 @@ const readFile = (path, req, res) => {
 
 const server = http.createServer((req, res) => {
     if (req.method === requisitionMethods.get) {
-        let fullPath = "./views/";
-
-        let path = req.url;
-
-        if (req.url.at(-1) === "/") {
-            path += "index.html";
+        if (req.url.includes("..")) {
+            res.writeHead(400, {"Content-type": "text/plain"});
+            res.write("Error 400: Bad Request");
+            res.end();
+        } else {
+            readFile(buildPath(req, res), req, res);
         }
-
-        readFile(fullPath + path, req, res);
     }
 });
 
