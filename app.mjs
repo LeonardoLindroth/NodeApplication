9f8dd0a0eb3264ae3a2764d9dd39a85bb5b785dc
LeonardoLindroth <leolindroth14@gmail.com>
2023-01-26 18:40:27 -0500

    creating method to updateData

--- app.mjs
+++ app.mjs
@@ -53,6 +53,35 @@ const saveData = (addingData, req, res) => {
     })
 }
 
+const updateData = (updatingData, req, res) => {
+    let savedDataJSON = getSavedData();
+
+    let url = req.url.split("/");
+
+    savedDataJSON[url[1]+"s"] = savedDataJSON[url[1]+"s"].map((item) => {
+        if (item.id === parseInt(updatingData.id, 10)) {
+            updatingData.id = parseInt(updatingData.id, 10);
+            return updatingData;
+        }
+
+        return item;
+    });
+
+    let insertData = JSON.stringify(savedDataJSON);
+
+    fs.writeFile("./app.json", insertData, (error) => {
+        if (error) {
+            res.writeHead(404, {"Content-Type": "application/json"});
+            res.write(JSON.stringify({ success: false, message: "Erro" }));
+            res.end();
+        } else {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.write(JSON.stringify({ success: true, message: "Valor atualizado!" }));
+            res.end();
+        }
+    })
+}
+
 const handlePost = (req, res) => {
     let bodyData = "";
 
@@ -75,7 +104,7 @@ const handlePost = (req, res) => {
             case "add":
                 saveData(objectData, req, res);
             case "update":
-                // updateData
+                updateData(objectData, req, res);
             case "delete":
                 // deleteData
             case "default":
